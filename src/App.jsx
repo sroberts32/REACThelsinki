15fc9d35da899232aa2a23ab8a593e961056b21e
unknown <crimm_32@hotmail.com>
2022-04-18 09:37:33 -0400

    1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,66 @@
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+	const course = 'Half Stack application development'
+	const parts = [
+		{
+		  name: 'Fundamentals of React',
+		  exercises: 10
+		},
+		{
+		  name: 'Using props to pass data',
+		  exercises: 7
+		},
+		{
+		  name: 'State of a component',
+		  exercises: 14
+		}
+	]
 
-function App(props) {
-  return (
-    <div>
-      <Header />
-      <Content />
-      <Total />
-    </div>
-  );
-}
+	return (
+		<div>
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total parts={parts} />
+		</div>
+	)
+};
+
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.course}</h1>
+		</div>
+	)
+};
+
+const Content = (props) => {
+	const lists = props.parts.map(function(item) {
+		return (
+			<div>
+				<p> {item.name}: {item.exercises}</p>
+			</div>
+		)
+	})
+	
+	return lists
+};
+
+const Total = (props) => {
+	var score = 0
+	
+	const lists = props.parts.map(function(item) {
+		
+		score = score + item.exercises 
+		
+	})
+	
+	return (
+		<div>
+			<p>Number of exercises: {score} </p>
+		</div>
+	)
+};
 
 export default App;
